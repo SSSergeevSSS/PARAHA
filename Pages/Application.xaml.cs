d2be8033a02ec1109f82904a8b4f499b902abfdd
Сергей Сергеевич <117578384+SSSergeevSSS@users.noreply.github.com>
2023-06-05 20:16:34 +0300

    Add files via upload

---
+++ Pages/Application.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Administration.Entity;
+using Administration.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Administration.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Applications.xaml
+    /// </summary>
+    public partial class Applications : Page
+    {
+        public Applications()
+        {
+            InitializeComponent();
+            DGrid.ItemsSource = AdministrationEntities.GetContext().Applications.ToList();
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Menu());
+        }
+    }
+}
---
+++ Pages/Menu.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Administration.Classes;
+using Administration.Pages;
+
+namespace Administration.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+        private void PopulationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Population());
+        }
+
+        private void StaffBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Staff());
+        }
+
+        private void ApplicationsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Application());
+        }
+    
+        private void AdministrationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Administration());
+        }
+
+
+        private void MenuFrame_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
